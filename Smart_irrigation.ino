7f7437794b3f3eb5aca07e26a01311afbf220f9b
damini-ai <79156063+damini-ai@users.noreply.github.com>
2021-04-15 14:54:08 +0530

    Update Smart_irrigation.ino

--- Smart_irrigation.ino
+++ Smart_irrigation.ino
@@ -1,6 +1,6 @@
 void setup() {
 
-pinMode(A0,INPUT_PULLUP);
+pinMode(A0,INPUT_PULLUP);// soil moisture sensor
 pinMode(8,OUTPUT);// buzzer
 pinMode(7,OUTPUT);//pump
 
@@ -13,7 +13,7 @@ Serial.begin(9600);
 void loop()
 {char Incoming_value = 0;
 int m=analogRead(A0); // to measure moisture in the soil
-Serial.println(m);
+Serial.println(m);  //to print the value of moisture
 delay(1000);
 
 if(m<=900)
@@ -24,12 +24,12 @@ Incoming_value = Serial.read();      //Read the incoming data and store it into 
     Serial.print("\n");        //New line 
     if(Incoming_value == 'a')            //Checks whether value of Incoming_value is equal to a
      { digitalWrite(7, HIGH);  //If value is a then pump turns ON
-      digitalWrite(8,HIGH);
+      digitalWrite(8,HIGH);  // buzzer beeps for 2 seconds
       delay(2000);
-      digitalWrite(8,LOW);}
+      digitalWrite(8,LOW);}  
     else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
       {digitalWrite(7, LOW);   //If value is 0 then pump turns OFF
-       digitalWrite(8,LOW);}
+       digitalWrite(8,LOW);}   //buzzer also turns off
 
 }
 else                    
