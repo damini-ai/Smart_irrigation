875a8fd7940c32b9dab74e534affb6a4655c2208
damini-ai <79156063+damini-ai@users.noreply.github.com>
2021-04-15 14:49:30 +0530

    Add files via upload

---
+++ Smart_irrigation.ino
@@ -0,0 +1,67 @@
+void setup() {
+
+pinMode(A0,INPUT_PULLUP);
+pinMode(8,OUTPUT);// buzzer
+pinMode(7,OUTPUT);//pump
+
+Serial.begin(9600);
+
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{char Incoming_value = 0;
+int m=analogRead(A0); // to measure moisture in the soil
+Serial.println(m);
+delay(1000);
+
+if(m<=900)
+{
+
+Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
+    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
+    Serial.print("\n");        //New line 
+    if(Incoming_value == 'a')            //Checks whether value of Incoming_value is equal to a
+     { digitalWrite(7, HIGH);  //If value is a then pump turns ON
+      digitalWrite(8,HIGH);
+      delay(2000);
+      digitalWrite(8,LOW);}
+    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
+      {digitalWrite(7, LOW);   //If value is 0 then pump turns OFF
+       digitalWrite(8,LOW);}
+
+}
+else                    
+  {
+    Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
+    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
+    Serial.print("\n");        
+   
+    
+    if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1 
+     { digitalWrite(7, HIGH);  //If value is 1 then pump turns ON
+      digitalWrite(8,HIGH);
+      delay(500);
+      digitalWrite(8,LOW);
+      }
+     else if(Incoming_value == 'a')       //Checks whether value of Incoming_value is equal to 0
+      {
+       digitalWrite(7, HIGH);             //If value is 0 then pump turns OFF
+       digitalWrite(8,HIGH);
+       delay(1000);
+       digitalWrite(8,LOW);
+       }
+   
+
+
+    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
+      {
+        digitalWrite(7, LOW);            //If value is 0 then pump turns OFF
+       digitalWrite(8,LOW);
+       }
+   
+       
+
+  }                           
+}
